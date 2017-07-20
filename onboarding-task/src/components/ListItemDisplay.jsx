40ef379f2131e099d26235d58f569fb5647ce461
Patrik Medveď <PatrikM@kentico.com>
2017-07-20 14:51:26 +0200

    Siplify component

--- onboarding-task/src/components/ListItemDisplay.jsx
+++ onboarding-task/src/components/ListItemDisplay.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export function ListItemDisplay(props) {
+export const ListItemDisplay = (props) => {
   return (
     <span onClick={props.onClick}>{props.itemName}</span>
   );
-}
+};
 
 ListItemDisplay.displayName = 'ListItemDisplay';
 
