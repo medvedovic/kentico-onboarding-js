072213100f1c5e9f6c8e395d31f329c1eb994d15
Patrik Medveď <PatrikM@kentico.com>
2017-07-25 13:13:57 +0200

    KA-161 Add export to actions

--- onboarding-task/src/actions/actionTypes.js
+++ onboarding-task/src/actions/actionTypes.js
@@ -1,9 +1,9 @@
 /**
  * Item manipulation
  */
-const CREATE_ITEM = 'CREATE_ITEM';
-const UPDATE_ITEM = 'UPDATE_ITEM';
-const DELETE_ITEM = 'DELETE_ITEM';
+export const CREATE_ITEM = 'CREATE_ITEM';
+export const UPDATE_ITEM = 'UPDATE_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
 
 /**
  * Action creators
