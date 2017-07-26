d9d78f8fe3e58a0d3ef56d1adda35573be455c94
Patrik Medveď <PatrikM@kentico.com>
2017-07-26 15:36:34 +0200

    KA-161 Remove unecessaty imports

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { OrderedMap } from 'immutable';
 
-import { ListItem } from './ListItem.jsx';
 import { ListItemContainer } from '../containers/ListItemContainer';
 import { ListItemInput } from './ListItemInput';
 
