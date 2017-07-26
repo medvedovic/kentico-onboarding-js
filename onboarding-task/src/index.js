b3238770f40273e0ef9f57cfaaabfe2ccdec99e9
Patrik Medveď <PatrikM@kentico.com>
2017-07-26 14:24:47 +0200

    KA161 Move store to index.js

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { OrderedMap } from 'immutable';
+import { createLogger } from 'redux-logger';
+
 import { App } from './App.jsx';
-import { store } from './containers/store';
+import { reducer } from './reducers/reducers';
+import { ListItemModel } from './model/ListItemModel';
+import { generateGuid } from './utils/generateGuid';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+
+const logger = createLogger();
+const initialState = {
+  items: new OrderedMap([
+    [generateGuid(), new ListItemModel({ value: 'Make coffee' })],
+    [generateGuid(), new ListItemModel({ value: 'Master React' })],
+  ]),
+};
+
+const store = createStore(reducer, initialState, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
