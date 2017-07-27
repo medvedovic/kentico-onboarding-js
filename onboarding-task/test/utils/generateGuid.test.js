760e3537419640d81423201256bfff51fca1f7e9
Patrik Medveď <PatrikM@kentico.com>
2017-07-27 12:02:09 +0200

    KA-160 Rewrite tests for readability

--- onboarding-task/test/utils/generateGuid.test.js
+++ onboarding-task/test/utils/generateGuid.test.js
@@ -2,23 +2,30 @@ import { generateGuid } from '../../src/utils/generateGuid';
 
 describe('generateGuid', () => {
   it('provides correct format', () => {
-    expect(/(\w{8}-\w{4}-4\w{3}-\w{4}-\w{12})/.test(generateGuid())).toBe(true);
+    const regex = /(\w{8}-\w{4}-4\w{3}-\w{4}-\w{12})/;
+    const guid = generateGuid();
+
+    const result = regex.test(guid);
+
+    expect(result).toBe(true);
   });
 
   it('provides correct length of guid', () => {
-    expect(generateGuid().length).toEqual(36);
+    const result = generateGuid().length;
+
+    expect(result).toEqual(36);
   });
 
   it('provides unique values', () => {
     const numberOfIterations = 100;
     let arr = [];
+
     for (let i = 0; i < numberOfIterations; i++) {
       arr.push(generateGuid());
     }
-    const newArr = arr.filter((value, index, self) => {
-      return self.indexOf(value) === index;
-    });
+    const set = new Set(arr);
     arr = null;
-    expect(newArr.length).toBe(numberOfIterations);
+
+    expect(set.size).toBe(numberOfIterations);
   });
 });
--- onboarding-task/test/utils/isTextInputValid.test.js
+++ onboarding-task/test/utils/isTextInputValid.test.js
@@ -2,18 +2,32 @@ import { isTextInputValid } from '../../src/utils/isTextInputValid';
 
 describe('isTextInputValid', () => {
   it('returns false for whitespaces', () => {
-    expect(isTextInputValid('    ')).toBe(false);
+    const result = isTextInputValid('     ');
+
+    expect(result).toBe(false);
   });
+
   it('returns false for null', () => {
-    expect(isTextInputValid(null)).toBe(false);
+    const result = isTextInputValid(null);
+
+    expect(result).toBe(false);
   });
+
   it('returns false for undefined', () => {
-    expect(isTextInputValid(undefined)).toBe(false);
+    const result = isTextInputValid(undefined);
+
+    expect(result).toBe(false);
   });
+
   it('returns false for empty strings', () => {
-    expect(isTextInputValid('')).toBe(false);
+    const result = isTextInputValid('');
+
+    expect(result).toBe(false);
   });
+
   it('returns true for a valid string', () => {
-    expect(isTextInputValid('Hello world')).toBe(true);
+    const result = isTextInputValid('Hello world');
+
+    expect(result).toBe(true);
   });
 });
