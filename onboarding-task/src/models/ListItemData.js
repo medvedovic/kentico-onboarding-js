46a7ce56b862515037b59b8c7f39326e72a6f62a
Patrik Medveď <PatrikM@kentico.com>
2017-08-01 15:20:53 +0200

    KA-161 Extract default values from records

--- onboarding-task/src/models/ListItemData.js
+++ onboarding-task/src/models/ListItemData.js
@@ -1,10 +1,11 @@
 import { Record } from 'immutable';
 
+const defaultValues = {
+  guid: '',
+  value: '',
+};
+
 /**
  * Represents a single item in the list
  */
-
-export const ListItemData = Record({
-  guid: '',
-  value: '',
-});
+export const ListItemData = Record(defaultValues);
