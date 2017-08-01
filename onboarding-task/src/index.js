e56723dc555199af8d7f52d1b84b889f15e704c6
Patrik Medveď <PatrikM@kentico.com>
2017-08-01 14:09:57 +0200

    KA-161 Configure redux dev tools

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 
 import { App } from './App.jsx';
@@ -11,8 +11,16 @@ import { initialState } from './constants/initialState';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const logger = createLogger();
-const store = createStore(rootReducer, initialState, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
