6117adf9af02621e5eea5359831e01cccd1f062b
Patrik Medveď <PatrikM@kentico.com>
2017-08-09 14:01:13 +0200

    KA-161 Rewrite flag reducer

---
+++ onboarding-task/test/reducers/items/flags/flag.test.js
@@ -0,0 +1,29 @@
+import { ListItemFlag } from '../../../../src/models/ListItemFlag';
+import { flag } from '../../../../src/reducers/items/flags/flag';
+import { createItem, toggleBeingEdited } from '../../../../src/actions/userActions';
+
+describe('Flag Reducer', () => {
+  it('returns new flags on create item', () => {
+    const test = flag(undefined, createItem('Make a sandwich'));
+
+    expect(test).toEqual(new ListItemFlag());
+  });
+
+  it('toggles being edited properly', () => {
+    const expectedState = new ListItemFlag({
+      isBeingEdited: true,
+    });
+
+    const newState = flag(new ListItemFlag(), toggleBeingEdited(''));
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('returns default state on wrong input', () => {
+    const expectedState = new ListItemFlag();
+
+    const test = flag(undefined, createItem(''));
+
+    expect(test).toEqual(expectedState);
+  });
+});
