175fa78ffcaba09d331db7550163f9fc1900819f
Patrik Medveď <PatrikM@kentico.com>
2017-08-09 13:26:29 +0200

    KA-161 Rewrite item factory test

--- onboarding-task/test/utils/itemFactory.test.js
+++ onboarding-task/test/utils/itemFactory.test.js
@@ -1,10 +1,19 @@
 import { itemFactory } from '../../src/utils/itemFactory';
+import { ListItemData } from '../../src/models/ListItemData';
+import { generateGuid } from '../../src/utils/generateGuid';
 
 describe('Item factory', () => {
-  it('no two items has the same id', () => {
-    const newItem = itemFactory('Make a sandwich');
-    const newItem2 = itemFactory('Make a sandwich');
+  it('returns new ListItemData correctly', () => {
+    const dummyIdGenerator = () =>
+      '9451e1f0-0279-4910-905f-05099d566ec6';
 
-    expect(newItem.id).not.toEqual(newItem2.id);
+    const expectedResult = new ListItemData({
+      id: '9451e1f0-0279-4910-905f-05099d566ec6',
+      value: 'Make a sandwich',
+    });
+
+    const newItem = itemFactory(dummyIdGenerator)('Make a sandwich');
+
+    expect(newItem).toEqual(expectedResult);
   });
 });
