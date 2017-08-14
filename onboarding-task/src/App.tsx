699611211c8fde71748f6d3b16f34d03ccc6a72a
Patrik Medveď <PatrikM@kentico.com>
2017-08-14 15:47:17 +0200

    KA-162 Change app file to ts

--- onboarding-task/src/App.tsx
+++ onboarding-task/src/App.tsx
@@ -1,8 +1,8 @@
 import './styles/sticky-footer.css';
-import React from 'react';
-import { List } from './containers/List.tsx';
+import * as React from 'react';
+import { List } from './containers/List';
 
-export const App = () => (
+export const App: React.SFC = () => (
   <div>
     <div className="container">
       <div className="header clearfix">
