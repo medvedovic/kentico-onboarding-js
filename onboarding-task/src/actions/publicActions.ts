e9aa52011bc18f19caf1c7ecb63d3aab0df1d13a
Patrik Medveď <PatrikM@kentico.com>
2017-08-24 09:53:16 +0200

    KA-162 Re-format public actions

--- onboarding-task/src/actions/publicActions.ts
+++ onboarding-task/src/actions/publicActions.ts
@@ -2,4 +2,8 @@ import { createItemBuilder } from './actionCreators';
 import { itemFactory } from '../utils/itemFactory';
 
 export const createItem = createItemBuilder(itemFactory);
-export { updateItem, deleteItem, toggleBeingEdited } from './userActions';
+export {
+  updateItem,
+  deleteItem,
+  toggleBeingEdited
+} from './userActions';
