488c69eec8277457c264ae978d354a8432fe1e1c
Patrik Medveď <PatrikM@kentico.com>
2017-08-30 15:17:26 +0200

    KA-196 Create fetch data action types

--- onboarding-task/src/constants/actionTypes.ts
+++ onboarding-task/src/constants/actionTypes.ts
@@ -2,3 +2,8 @@ export const CREATE_ITEM = 'CREATE_ITEM';
 export const UPDATE_ITEM = 'UPDATE_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
 export const TOGGLE_BEING_EDITED = 'TOGGLE_BEING_EDITED';
+export namespace FetchData {
+  export const IS_LOADING = 'IS_LOADING';
+  export const HAS_FAILED = 'HAS_FAILED';
+  export const HAS_SUCCEEDED = 'HAS_SUCCEEDED';
+}
