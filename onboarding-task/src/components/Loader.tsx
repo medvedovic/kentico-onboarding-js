ffd2bdf7e0ddc7cae74a0a7d74bf250f43c43cb6
Patrik Medveď <PatrikM@kentico.com>
2017-08-31 15:59:47 +0200

    KA-196 Restyle loader

--- onboarding-task/src/components/Loader.tsx
+++ onboarding-task/src/components/Loader.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
 export const Loader: React.SFC = () => (
-  <div className="loader"></div>
+  <div className="loader-wrapper">
+    <div className="loader" />
+  </div>
 );
