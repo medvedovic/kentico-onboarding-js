c7311dc5a69f6f4f84c61cc237760e2c0accc1fa
Patrik Medveď <PatrikM@kentico.com>
2017-09-05 10:07:18 +0200

    KA-196 Write tests for app settings reducer

---
+++ onboarding-task/test/reducers/app/settings.test.ts
@@ -0,0 +1,54 @@
+import { settings } from '../../../src/reducers/app/settings';
+import {
+  fetchHasFailed,
+  fetchHasSucceeded,
+  fetchStartLoading,
+  fetchStopLoading
+} from '../../../src/actions/fetchActions';
+import { AppSettings } from '../../../src/constants/AppSettings';
+import { deleteItem } from '../../../src/actions/userActions';
+
+describe('Settings reducer', () => {
+
+  it('Sets loader on correctly', () => {
+    const initialState = new AppSettings();
+
+    const result = settings(initialState, fetchStartLoading());
+
+    expect(result.showLoader).toBeTruthy();
+  });
+
+  it('Stops loader', () => {
+    const initialState = new AppSettings({
+      showLoader: true,
+    });
+
+    const result = settings(initialState, fetchStopLoading());
+
+    expect(result.showLoader).toBeFalsy();
+  });
+
+  it('Sets correct state on failure', () => {
+    const initialState = new AppSettings();
+
+    const result = settings(initialState, fetchHasFailed('Error message'));
+
+    expect(result.fetchHasFailed).toBeTruthy();
+  });
+
+  it('Sets correct state on success', () => {
+    const initialState = new AppSettings();
+
+    const result = settings(initialState, fetchHasSucceeded([]));
+
+    expect(result.fetchHasFailed).toBeFalsy();
+  });
+
+  it('Returns default state', () => {
+    const expectedState = new AppSettings();
+
+    const result = settings(undefined, deleteItem(''));
+
+    expect(result).toEqual(expectedState);
+  });
+});
