fa589e90226850b538e222bec423fcfa81324f19
Patrik Medveď <PatrikM@kentico.com>
2017-09-21 16:22:58 +0200

    KA-196 Write success case test for fetchDataActionFactory

---
+++ onboarding-task/test/actions/fetchDataActionFactory.test.ts
@@ -0,0 +1,56 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { fetchDataActionFactory } from '../../src/actions/fetchDataActionFactory';
+import { Promise } from 'es6-promise';
+import { FetchData } from '../../src/constants/actionTypes';
+import { IItemDataDTO } from '../../src/models/ItemDataDTO';
+import { fetchHasFailed } from '../../src/actions/fetchActions';
+import {
+  fetchStartLoading,
+  fetchStopLoading
+} from '../../src/actions/publicActions';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const items = [
+  { 'id': 1, 'value': 'Do stuff' },
+  { 'id': 2, 'value': 'Go Home' }
+];
+const mockSuccessPromise = (url: string) => Promise.resolve(
+  new Response(JSON.stringify(items))
+);
+const startLoader = () => fetchStartLoading();
+const stopLoader = () => fetchStopLoading();
+const onFetchSucceeded = (input: Array<IItemDataDTO>) => ({
+  type: FetchData.HAS_SUCCEEDED,
+  payload: {
+    items: input
+  }
+});
+const onFetchFailed = (error: Error) => fetchHasFailed(error);
+
+describe('fetchDataActionFactory', () => {
+  it('dispatches correct actions', () => {
+    const dependencies = {
+      fetchOperation: mockSuccessPromise,
+      startLoader,
+      stopLoader,
+      onFetchSucceeded,
+      onFetchFailed,
+    };
+    const store = mockStore({});
+    const expectedResult = [
+      { type: 'IS_LOADING', payload: { isLoading: true } },
+      { type: 'HAS_SUCCEEDED', payload: { items } },
+      { type: 'IS_LOADING', payload: { isLoading: false } }
+    ];
+
+    return store.dispatch(fetchDataActionFactory(dependencies)(''))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions).toEqual(expectedResult);
+      });
+  });
+});
