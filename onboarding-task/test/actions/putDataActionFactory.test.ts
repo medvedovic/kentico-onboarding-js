882654fbd6942bc956e094c3a734b2fb808cbe3f
Patrik Medveď <PatrikM@kentico.com>
2017-09-27 14:13:11 +0200

    KA-196 Write test for unsuccessfull put operation

--- onboarding-task/test/actions/putDataActionFactory.test.ts
+++ onboarding-task/test/actions/putDataActionFactory.test.ts
@@ -10,22 +10,34 @@ import {
 } from '../../src/constants/actionTypes';
 import { OrderedMap } from 'immutable';
 
+
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
+
+let mockInitialState = () => ({
+  items: {
+    data: OrderedMap([['1234', { id: '1234', value: 'Do stuff' }]])
+  }
+});
+
 const mockPutSuccess = (_url: string, _id: string, _value: string) => Promise.resolve(
-  new Response()
+  new Response(JSON.stringify({id: '', value: ''}))
+);
+const mockPutError = (_url: string, _id: string, _value: string) => Promise.reject(
+  new Error('Some nasty shit happened')
 );
 const onPutSuccess = (_localId: string, _response: IItemDataDTO) => ({
   type: HttpAction.PUT,
   status: EHttpActionStatus.success,
-  payload: undefined
+  payload: 'success'
 });
 const onPutError = (_localId: string, _response: Error) => ({
   type: HttpAction.PUT,
-  status: EHttpActionStatus.success,
-  payload: undefined
+  status: EHttpActionStatus.error,
+  payload: 'Some nasty shit happened'
 });
+
 const updateItemOperation = (localId: string, value: string) => ({
   type: UPDATE_ITEM,
   payload: {
@@ -34,14 +46,10 @@ const updateItemOperation = (localId: string, value: string) => ({
   }
 });
 
+
 describe('putDataActionFactory', () => {
   it('returns correct actions on success', () => {
-    let mockInitialState = {
-      items: {
-        data: OrderedMap([['1234', { value: 'Do stuff' }]])
-      }
-    };
-    const store = mockStore(mockInitialState);
+    const store = mockStore(mockInitialState());
     const dependencies = {
       putOperation: mockPutSuccess,
       onPutSuccess,
@@ -58,14 +66,37 @@ describe('putDataActionFactory', () => {
     const putSuccessResult = {
       type: HttpAction.PUT,
       status: EHttpActionStatus.success,
-      payload: undefined
+      payload: 'success'
     };
+
 
     return store.dispatch(putDataActionFactory(dependencies)('url', '1234', 'Go home'))
       .then(() => {
         const actions = store.getActions();
-        expect(actions).toContainEqual(putSuccessResult);
         expect(actions).toContainEqual(updateResult);
+        expect(actions).toContainEqual(putSuccessResult);
+      });
+  });
+
+  it('returns correct actions on failure', () => {
+    const store = mockStore(mockInitialState());
+    const dependencies = {
+      putOperation: mockPutError,
+      onPutSuccess,
+      onPutError,
+      updateItemOperation
+    };
+    const expectedResult = {
+      type: HttpAction.PUT,
+      status: EHttpActionStatus.error,
+      payload: 'Some nasty shit happened'
+    };
+
+
+    return store.dispatch(putDataActionFactory(dependencies)('url', '1234', 'Go home'))
+      .catch(() => {
+        const actions = store.getActions();
+        expect(actions).toContainEqual(expectedResult);
       });
   });
 });
