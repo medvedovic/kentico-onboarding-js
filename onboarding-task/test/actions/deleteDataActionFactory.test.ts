e68445dca6ee071ef73b32d8e972e12d6d910b0e
Patrik Medveď <PatrikM@kentico.com>
2017-09-29 09:26:38 +0200

    KA-196 Write test for unsuccessfull delete operation

--- onboarding-task/test/actions/deleteDataActionFactory.test.ts
+++ onboarding-task/test/actions/deleteDataActionFactory.test.ts
@@ -22,6 +22,9 @@ const mockInitialState = () => ({
 const mockDeleteSuccess = (_url: string, _id: number) => Promise.resolve(
   new Response()
 );
+const mockDeleteError = (_url: string, _id: number) => Promise.reject(
+  'Error occurred'
+);
 const onDeleteError = (_localId: string, _response: Error) => ({
   type: HttpAction.DELETE,
   status: EHttpActionStatus.error,
@@ -41,7 +44,7 @@ describe('deleteDataActionFactory', () => {
       onDeleteSuccess,
       onDeleteError
     };
-    const expextedAction =  {
+    const expextedAction = {
       type: DELETE_ITEM,
       payload: '1234'
     };
@@ -51,6 +54,27 @@ describe('deleteDataActionFactory', () => {
       .then(() => {
         const actions = store.getActions();
         expect(actions).toContainEqual(expextedAction);
+      });
+  });
+
+  it('returns correct actions on failure', () => {
+    const store = mockStore(mockInitialState());
+    const expectedAction = {
+      type: HttpAction.DELETE,
+      status: EHttpActionStatus.error,
+      payload: 'Error occurred'
+    };
+    const dependencies = {
+      deleteOperation: mockDeleteError,
+      onDeleteSuccess,
+      onDeleteError
+    };
+
+
+    return store.dispatch(deleteDataActionFactory(dependencies)('url', '1234'))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions).toContainEqual(expectedAction);
       });
   });
 });
