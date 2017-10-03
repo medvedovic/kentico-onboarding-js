72aa542b91cbb5dac3315f599a28f6f3116aca9e
Patrik Medveď <PatrikM@kentico.com>
2017-10-03 13:53:32 +0200

    KA-196 Inject fetch to httpActionBuilder as dependency

--- onboarding-task/src/actions/httpActionBuilder.ts
+++ onboarding-task/src/actions/httpActionBuilder.ts
@@ -1,33 +1,34 @@
 import { HttpAction } from '../constants/actionTypes';
 import { toItemDataDTO } from '../models/ItemDataDTO';
 
-const fetch = require('isomorphic-fetch');
+const { fetch } = require('isomorphic-fetch');
 
-const httpActionBuilder = (url: string, httpMethod: string, body?: any) =>
-  fetch(url, {
-    method: httpMethod,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body
-  })
-    .then((response: Response) => {
-      if (!response.ok)
-        throw new Error(response.statusText + ' at ' + response.url);
-      return response;
-    });
+export const httpActionBuilder = (injectedFetch: (url: string, init?: any) => Promise<Response>) =>
+  (url: string, httpMethod: string, body?: any) =>
+    injectedFetch(url, {
+      method: httpMethod,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then((response: Response) => {
+        if (!response.ok)
+          throw new Error(response.statusText + ' at ' + response.url);
+        return response;
+      });
 
 export const deleteAction = (url: string, id: string) =>
-  httpActionBuilder(`${url}/${id}`, HttpAction.DELETE);
+  httpActionBuilder(fetch)(`${url}/${id}`, HttpAction.DELETE);
 
 export const postAction = (url: string, value: string) => {
   const itemDto = toItemDataDTO(value);
 
-  return httpActionBuilder(url, HttpAction.POST, JSON.stringify(itemDto));
+  return httpActionBuilder(fetch)(url, HttpAction.POST, JSON.stringify(itemDto));
 };
 
 export const putAction = (url: string, id: string, value: string) => {
   const itemDto = toItemDataDTO(value, id);
 
-  return httpActionBuilder(`${url}/${id}`, HttpAction.PUT, JSON.stringify(itemDto));
+  return httpActionBuilder(fetch)(`${url}/${id}`, HttpAction.PUT, JSON.stringify(itemDto));
 };
