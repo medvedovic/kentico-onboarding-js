eafadc63b60887686d0155899fa72ace31ca6dd7
Patrik Medveď <PatrikM@kentico.com>
2017-10-09 18:54:22 +0200

    KA-196 Rewrite test for toItemDataDto converter

--- onboarding-task/test/models/toItemDataDTO.test.ts
+++ onboarding-task/test/models/toItemDataDTO.test.ts
@@ -2,26 +2,21 @@ import {
   IItemDataDTO,
   toItemDataDTO
 } from '../../src/models/ItemDataDTO';
+import { ListItemData } from '../../src/models/ListItemData';
 
 describe('toItemDataDto', () => {
-  it('Converts value to itemDto correctly', () => {
+  it('Converts ListItemData to itemDto correctly', () => {
+    const testItem = new ListItemData({
+      id: '00000000-0000-0000-0000-000000000000',
+      value: 'Go home'
+    });
     const expectedResult: IItemDataDTO = {
       value: 'Go home',
       id: '00000000-0000-0000-0000-000000000000'
     };
-    const result = toItemDataDTO('Go home', '00000000-0000-0000-0000-000000000000');
+
+    const result = toItemDataDTO(testItem);
 
     expect(expectedResult).toEqual(result);
-  });
-
-  it('Sets default id', () => {
-    const expectedResult: IItemDataDTO = {
-      value: 'Go home',
-      id: '00000000-0000-0000-0000-000000000000'
-    };
-
-    const result = toItemDataDTO('Go home');
-
-    expect(result).toEqual(expectedResult);
   });
 });
