46a4eac50d59f7969ad71a49acfd43e94f47dd15
Patrik Medveď <PatrikM@kentico.com>
2017-10-12 13:02:04 +0200

    KA-196 Add missing tests for list reducer

--- onboarding-task/test/reducers/items/flags/flags.test.js
+++ onboarding-task/test/reducers/items/flags/flags.test.js
@@ -1,9 +1,15 @@
 import { Map } from 'immutable';
-import { flags } from '../../../../src/reducers/items/flags/flags';
-import { deleteItem, toggleBeingEdited } from '../../../../src/actions/publicActions';
-import { createItemBuilder } from '../../../../src/actions/actionCreators';
-import { ListItemFlags } from '../../../../src/models/ListItemFlags';
-import { ListItemData } from '../../../../src/models/ListItemData';
+import { flags } from '../../../../src/reducers/items/flags/flags.ts';
+import {
+  deleteItem,
+  toggleBeingEdited,
+} from '../../../../src/actions/publicActions.ts';
+import {
+  createItemBuilder,
+  fetchHasSucceededBuilder,
+} from '../../../../src/actions/actionCreators.ts';
+import { ListItemFlags } from '../../../../src/models/ListItemFlags.ts';
+import { ListItemData } from '../../../../src/models/ListItemData.ts';
 
 describe('Flags reducer', () => {
   const id1 = 'e9082417-eae0-4b00-a2d0-5722ba3b1641';
@@ -54,5 +60,23 @@ describe('Flags reducer', () => {
     const test = flags(undefined, {});
 
     expect(test).toEqual(new Map());
+  });
+
+  it('creates new flags for items after fetch correctly', () => {
+    const mockResponse = [
+      { id: id1, value: 'Make coffee' },
+      { id: id2, value: 'Master React' },
+    ];
+    const expectedStoreState = new Map([
+      [id1, new ListItemFlags()],
+      [id2, new ListItemFlags()],
+    ]);
+
+    const mockParse = (value, id) => new ListItemData({ id, value, localId: id });
+    const fetch = fetchHasSucceededBuilder(mockParse)(mockResponse);
+
+    const testResult = flags(undefined, fetch);
+
+    expect(testResult).toEqual(expectedStoreState);
   });
 });
