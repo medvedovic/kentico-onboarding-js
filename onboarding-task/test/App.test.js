679cecb9c011b710545f14a0096d740132bc5382
Patrik Medveď <PatrikM@kentico.com>
2017-11-13 15:12:59 +0100

    KA-196 Fix test for App renders

--- onboarding-task/test/App.test.js
+++ onboarding-task/test/App.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from '../src/components/App.tsx';
+import { App } from '../src/containers/App.tsx';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from '../src/reducers/rootReducer';
 import { initialState } from '../src/constants/initialState';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, initialState);
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(thunk)
+);
 
-// This test is ignored as I have no idea how to make it work
-
-xit('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={store}>
     <App />
