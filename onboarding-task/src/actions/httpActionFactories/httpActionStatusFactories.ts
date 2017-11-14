8521ee9483d49b533547dcfa57f400c81825155b
Patrik Medveď <PatrikM@kentico.com>
2017-11-14 13:39:01 +0100

    KA-196 Create a separate handler for successful post

--- onboarding-task/src/actions/httpActionFactories/httpActionStatusFactories.ts
+++ onboarding-task/src/actions/httpActionFactories/httpActionStatusFactories.ts
@@ -1,13 +1,23 @@
 import { IServerItemDataModel } from '../../models/IServerItemDataModel';
 import { IAction } from '../IAction';
 import { ListItemData } from '../../models/ListItemData';
+import { POST_ITEM_TO_SERVER } from '../../constants/actionTypes';
 
+export const handleSuccessfulPost = (temporaryId: string, params: IServerItemDataModel) => ({
+  type: POST_ITEM_TO_SERVER.SUCCESS,
+  payload: {
+    temporaryId,
+    item: new ListItemData({
+      id: params.id,
+      value: params.value,
+    })
+  }
+});
 
 export const handleSuccessfulRequest = (type: string) =>
-  (id: string, params: IServerItemDataModel): IAction => ({
+  (params: IServerItemDataModel): IAction => ({
     type,
     payload: {
-      id,
       item: new ListItemData({
         id: params.id,
         value: params.value,
